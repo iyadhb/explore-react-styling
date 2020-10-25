0025283b91cc7b156280b8382e08f82298994822
Iyad <iyadhb@gmail.com>
2020-10-25 19:11:54 +0000

    advanced variables - css

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,15 +1,26 @@
-import React from 'react'
 import styled from 'styled-components'
 
+const color = 'red';
+const padding = "padding: 1rem";
+const margin = "margin: 1rem";
+
+const border = (width = '3px') => {
+    return `border:solid ${width} red`;
+}
+
 export const Button = styled.button`
+${padding};
+${margin};
 color: red;
 background: blue;
 text-transform: uppercase;
+${border('10px')};
 `;
 
 export const SecondButton = styled.button`
-color: blue;
-background: red;
-text-transform:uppercase
+color: ${color};
+background: var(--primaryColor);
+text-transform:uppercase;
+${border()};
 `
 
