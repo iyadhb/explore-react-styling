e9bdcb692e631258ff41366dc09678f912456d28
Iyad <iyadhb@gmail.com>
2020-10-25 20:27:42 +0000

    Global stules in styled compponents

--- src/App.js
+++ src/App.js
@@ -3,9 +3,25 @@ import './App.css';
 import StyledHeader from './components/StyledHeader'
 import PropsButton from './components/PropsButton'
 
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  body {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+* {
+  margin: 0;
+  box-sizing: border-box;
+}
+`
+
+
 function App() {
   return (
     <div>
+    <GlobalStyle />
+
     <StyledHeader title="Hello from app.js" />
     <PropsButton color='blue'>Button number one</PropsButton>
     <PropsButton color='red'>Button number two</PropsButton>
