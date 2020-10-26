bdb9354f758da7b8e5fb705572a2613d30a23716
Iyad <iyadhb@gmail.com>
2020-10-26 20:59:47 +0000

    Simple Media Queries start and clean up

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
-import { ThemeProvider } from 'styled-components'
-import { redTheme, greenTheme, blueTheme } from './components/Themes'
-import NavBar from './components/NavBar'
-import Button from './components/Button'
-import Content from './components/Content'
+
 
 function App() {
-const [ theme, setTheme ] = useState(redTheme)
 
-const handeTheme = () => {
-  if(theme.primaryColor === 'red') {
-    setTheme(greenTheme)
-  } else if (theme.primaryColor === 'green') {
-    setTheme(blueTheme)
-  } else if(theme.primaryColor === 'blue') {
-    setTheme(redTheme)
-  }
-}
 
   return (
-    <ThemeProvider theme={theme}>
-    <main>
-    <NavBar>
-    <Button onClick={handeTheme}>Toggle theme</Button>
-    
-    </NavBar>
-    <Content />
-    </main>
-    </ThemeProvider>
+    <div>
+    Hello from app
+    </div>
   );
 }
 
