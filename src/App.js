b9a691ae40abb249432bab673c2b6738ed26c463
Iyad <iyadhb@gmail.com>
2020-10-26 14:10:55 +0000

    attrs method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import './App.css';
-import Hero from './components/Hero'
-import img1 from './images/image-1.jpg'
-import img2 from './images/image-2.jpg'
-import img3 from './images/image-3.jpg'
-import Banner from './components/Banner'
-import {DangerButton} from './example/PropsButton'
+import { BasicInput, AwsomeInput } from './components/Inputs'
+
 
 
 
 function App() {
   return (
     <div>
-   <Hero big img={img1}> 
-   <Banner title="This is my title" color="orange">
-   <DangerButton big>Click Me</DangerButton>
-   </Banner>
-   </Hero>
-   <Hero big />
-   <Hero img={img2} />
-   <Hero img={img3} />
+   <BasicInput />
+   <AwsomeInput />
+   <AwsomeInput type='email' placeholder='enter your email' />
     
     </div>
   );
